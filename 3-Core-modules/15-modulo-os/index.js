fc411f641fa28029e38cbd0c8cc4e16ecd188c65
valdenio458 <valdeniomartinho@gmail.com>
2022-04-14 18:55:09 -0300

    Módulo os

---
+++ 3-Core-modules/15-modulo-os/index.js
@@ -0,0 +1,12 @@
+const os = require('os');
+
+console.log(os.cpus()); //* quantas cpus
+
+console.log(os.freemem()); //* quanto de memória
+
+console.log(os.homedir()); //* qual o diretório da home
+
+console.log(os.type()); //* qual sistema operacional está rodando
+
+
+
