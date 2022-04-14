d1f9dc697b3e63a960d5179fba1120f499baf64a
valdenio458 <valdeniomartinho@gmail.com>
2022-04-14 10:49:32 -0300

    Renomeando arquivos

---
+++ 3-Core-modules/9-renomear-arquivo/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const arqAntigo = 'arquivo.txt';
+const arqNovo = 'novo.txt';
+fs.rename(arqAntigo, arqNovo, function (err){
+  if(err){
+    console.log(err)
+    return
+  } else{
+    console.log(`O arquivo ${arqAntigo} foi renomeado para ${arqNovo}!`);
+  }
+})
