9b427fea053d3f494fdbb9c6e6105f34c1af4f85
valdenio458 <valdeniomartinho@gmail.com>
2022-04-17 10:03:49 -0300

    Adiciona css

---
+++ 6-express/9-css/index.js
@@ -0,0 +1,35 @@
+import express, { application } from 'express';
+import path from 'path';
+import router from './routes/user.js';
+const __dirname = path.resolve();
+
+const app = express();
+
+//* ler o body:
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+const PORT = 3000;
+
+//* arquivos estáticos:
+app.use(express.static(__dirname + '/public'));
+
+
+const basePath = path.join(__dirname, 'templates');
+
+app.use('/users', router)
+
+app.get('/', (_req, res) => {
+
+res.sendFile(`${basePath}/index.html`)
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Aplicação rodando na porta: ${PORT}`);
+});
