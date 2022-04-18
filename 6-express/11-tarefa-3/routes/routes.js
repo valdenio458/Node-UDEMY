0b7d5827a63336ccc7b4f0a844422cea38c07edd
valdenio458 <valdeniomartinho@gmail.com>
2022-04-18 10:58:14 -0300

    Coloca underline no parâmetro req

--- 6-express/11-tarefa-3/routes/routes.js
+++ 6-express/11-tarefa-3/routes/routes.js
@@ -5,15 +5,15 @@ const __dirname = path.resolve();
 
 const basePath = path.join(__dirname, './templates');
 
-router.get('/', (req, res) => {
-  res.sendFile(`${basePath}/index.html`); 
+router.get('/', (_req, res) => {
+  res.sendFile(`${basePath}/index.html`);   
 });
 
-router.get('/new', (req, res) => {
+router.get('/new', (_req, res) => {
     res.sendFile(`${basePath}/new.html`);
     });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (_req, res) => {
   res.sendFile(`${basePath}/project.html`);
 });
 
