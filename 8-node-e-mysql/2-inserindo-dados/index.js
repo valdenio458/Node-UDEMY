c5d2f44acef24452a16e306069070cbe773fcc63
valdenio458 <valdeniomartinho@gmail.com>
2022-04-20 19:05:19 -0300

    Inserindo dados

---
+++ 8-node-e-mysql/2-inserindo-dados/index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import exphbs from 'express-handlebars';
+import mysql from 'mysql';
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+  )
+
+  app.use(express.json()); 
+
+//* Setup do handlebars:
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars');
+
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (_req, res) => {
+  res.render('home');
+  });
+
+app.post('/books/insertbook', (req, res) => {
+  const { title, author} = req.body;
+  console.log('Xablau');
+
+  const QUERY = `INSERT INTO books (title, author) VALUES ('${title}', '${author}')`;
+
+  conn.query(QUERY, (err, _results) => {
+    if (err) {
+      console.log(err);
+      res.send('Erro ao inserir livro');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'flamengo61',
+  database: 'nodemysql2'
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Connected to MySql!');
+}),
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  })
+
+
