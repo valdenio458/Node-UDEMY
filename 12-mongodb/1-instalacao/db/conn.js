9b3783804285f8744a2bc2bbcd34f7f1eba296fd
valdenio458 <valdeniomartinho@gmail.com>
2022-05-13 11:56:28 -0300

    Setup mongodb

---
+++ 12-mongodb/1-instalacao/db/conn.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb://localhost:27017/testemongodb";
+
+const client = new MongoClient(uri);
+
+async function run () {
+    try {
+        await client.connect();
+        console.log('Conectado ao MongoDB!');
+}
+catch (err) {
+    console.log(err);
+}
+}
+
+run();
+
+export default client;
