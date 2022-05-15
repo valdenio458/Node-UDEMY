979becb62c91c903b0b85569bf44090c84037595
valdenio458 <valdeniomartinho@gmail.com>
2022-05-15 18:54:07 -0300

    resgatando dados

---
+++ 12-mongodb/4-resgatando-dados/routes/productsRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import ProductController from '../controllers/ProductController.js';
+
+let router = express.Router();
+
+router.get('/create', ProductController.createProduct);
+router.post('/create', ProductController.createProductPost);
+router.get('/', ProductController.showProducts);
+
+export default router;
